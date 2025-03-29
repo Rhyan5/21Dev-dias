f42574fc210b527bba85f6c08e3989b8086420b1
Rhyan Marllon <rhyanlk05@gmail.com>
2025-03-29 13:32:00 -0300

    colocando laço de repetição no programa

--- dia 9/index.js
+++ dia 9/index.js
@@ -1,11 +1,13 @@
 ///////CONHECENDO O USUÁRIO
+let continuar = false
+
+do{
 
 let nome = prompt("Qual seu nome?")
 let idade = Number(prompt("Qual sua idade?"))
-let peso = prompt("Qual seu peso")
-let altura = prompt("Qual sua altura?")
+let peso = Number(prompt("Qual seu peso"))
+let altura = Number(prompt("Qual sua altura?"))
 let profissao = prompt("Qual sua profissão?")
-
 console.log("Seu nome é "+nome+", tem "+idade+" anos, "+peso+"kg, tem "+altura+" de altura e é "+profissao+".")
 
 if (idade >= 18) {
@@ -25,16 +27,37 @@ console.log("Você já viveu "+meses+" meses, "+semanas+" semanas e "+dias+" dia
 imc = peso / (altura * altura)
 switch (imc){
     case imc <= 18.5:
-        console.log("Você está abaixo do peso.")
+        console.log("O seu IMC é "+imc+"kg/m2, você está abaixo do peso.")
         break;
     case imc > 18.5 && imc <= 24.9:
-        console.log("O seu peso está normal.")
+        console.log("O seu IMC é "+imc+"kg/m2, o seu peso está normal.")
         break;
     case imc > 24.9 && imc < 30:
-        console.log("Você está acima do peso.")
+        console.log("O seu IMC é "+imc+"kg/m2, você está acima do peso.")
         break;
     case imc >= 30:
-        console.log("Você está obeso")
+        console.log("O seu IMC é "+imc+"kg/m2, você está obeso")
         break;  
 }
+
+//// ANO NASCIMENTO
+ano = 2025 - idade
+console.log("Você nasceu no ano de "+ano+".")
+
+///// ANOS VIVIVOS
+anovivi = 2025 - idade
+idadeAtual = 0
+for (let i= anovivi; i <= 2025; i++){
+    console.log("Nasceu em "+i+" e viveu " +idadeAtual+" anos.")
+    idadeAtual++;
+}
+
+///// REPETIR O PROGRAMA
+    let inserir = prompt("Deseja inserir novos dados? (s/n)")
+    if (inserir == "s" || inserir == "S"){
+        continuar = true}
+    else{
+        alert("Programa encerrado!")
+        continuar = false}
+    }   while( continuar == true) 
 
